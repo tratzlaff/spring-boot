fb707939f72f66a4ff54619f737e3f7ae1611740
Tanner Ratzlaff <tratzlaff@users.noreply.github.com>
2015-09-22 14:31:15 -0700

    Add URL for "Mapping Requests With @RequestMapping" spring docs.

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * The @RequestMapping annotation maps / to the index() method. When invoked from a browser or using curl on the
  * command line, the method returns pure text. That’s because @RestController combines @Controller and @ResponseBody,
  * two annotations that results in web requests returning data rather than a view.
+ * 
+ * http://docs.spring.io/spring/docs/current/spring-framework-reference/htmlsingle/#mvc-ann-requestmapping
  */
 @RestController
 public class HelloController {
