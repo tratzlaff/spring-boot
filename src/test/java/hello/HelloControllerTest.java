efac41689606be51764f5a85f157e1d34449cf40
Tanner Ratzlaff <tratzlaff@netflix.com>
2017-05-09 14:04:46 -0500

    Upgrading to spring-boot-gradle-plugin 1.5.3 and Gradle 3.5 wrapper.

--- src/test/java/hello/HelloControllerTest.java
+++ src/test/java/hello/HelloControllerTest.java
@@ -4,36 +4,23 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockServletContext;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-/**
- * Note the use of the MockServletContext to set up an empty WebApplicationContext so the HelloController
- * can be created in the @Before and passed to MockMvcBuilders.standaloneSetup().
- * An alternative would be to create the full application context using the Application class and @Autowired the
- * HelloController into the test. The MockMvc comes from Spring Test and allows you, via a set of convenient
- * builder classes, to send HTTP requests into the DispatcherServlet and make assertions about the result.
- */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = MockServletContext.class)
-@WebAppConfiguration
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class HelloControllerTest {
 
+	@Autowired
 	private MockMvc mvc;
-
-	@Before
-	public void setUp() throws Exception {
-		mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
-	}
 
 	@Test
 	public void getHello() throws Exception {
